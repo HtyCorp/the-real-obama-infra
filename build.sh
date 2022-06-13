23a4706bfc1b9062145fd87ccf2be2ec02633b2f
Hamish Tym <hamishtym@bigpond.com>
2022-06-14 00:12:09 +1000

    Copy shadow JAR from service package build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 npm run build
-cp ../the-real-obama/build/libs/the-real-obama-1.0-SNAPSHOT.jar docker/service/service.jar
+cp ../the-real-obama/build/libs/the-real-obama-1.0-SNAPSHOT-all.jar docker/service/service.jar
 npx cdk synth
